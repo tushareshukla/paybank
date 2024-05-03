06ff1d3fcbd2201f8693690578eff51780bf7ccc
bhudev <tushareshukla@gmail.com>
2024-05-03 22:20:40 +0530

    middle-ware- user-schema-user-signup-login

---
+++ backend/routes/index.js
@@ -0,0 +1,9 @@
+//backend api express
+
+//backend/routes/index.js
+
+const express = require("express");
+const userRouter = require("./user");
+const router = express.Router();
+router.use("/user", userRouter)
+module.exports = router;
